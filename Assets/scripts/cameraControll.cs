f9f649d7fb59b80b152983970fbda9ca40944319
Allan Urique <31394670+southrad69@users.noreply.github.com>
2019-04-03 12:06:14 -0300

    Initial commit
    Added the project files with inputs and behaviours, level design is still needed

---
+++ Assets/scripts/cameraControll.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControll : MonoBehaviour
+{
+    public GameObject lookattarget, gototarget;
+    public float speed = 1;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+
+    void Update()
+    {
+        transform.LookAt(lookattarget.transform);
+
+        transform.position = Vector3.Lerp(transform.position, gototarget.transform.position, Time.deltaTime* speed);
+    }
+}
---
+++ Assets/scripts/enemyAI.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class enemyAI : MonoBehaviour
+{
+    
+    public ParticleSystem fire;
+    public bool enableshoot = false;
+    int index = 0;
+    float rotation = 0;
+    float patrolSpeed = 150;
+    public float radius = 10;
+    GameObject target;
+    public GameObject[] waypoints;
+
+
+    void Start()
+    {
+        target = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+
+        fire.Stop();
+        float dist = Vector3.Distance(target.transform.position, transform.position);
+
+        if (enableshoot == false){
+
+            if (rotation == 360){
+                rotation = 0;
+            }
+
+            rotation = rotation + radius * Time.deltaTime;
+            gameObject.transform.rotation = Quaternion.Euler(0, rotation, 0);
+            gameObject.transform.Translate(0, 0, patrolSpeed * Time.deltaTime);
+
+            print("Moving at patrol speed");
+        }
+
+        if (enableshoot)
+        {
+            fire.Emit(1);
+            transform.LookAt(target.transform.position + target.transform.forward * dist * 0.5f);
+
+            if (dist <= 30)
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waypoints[index].transform.position, Time.deltaTime * 20);
+
+                print("Decreasing speed");
+            }
+            else
+            {
+                transform.position = Vector3.MoveTowards(transform.position, waypoints[index].transform.position, Time.deltaTime * 80);
+                print("Moving at engagement speed");
+            }
+
+        }     
+
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {        
+        enableshoot = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        fire.Stop();
+        enableshoot = false;
+    }
+
+}
+
